f9bf30cd183f817146ceec137b6abcc0d03f76d5
Saeed-Abdalwahab <nayifattamwel@gmail.com>
2025-08-06 11:12:17 +0300

    change number

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+callNumber() {
+  window.location.href = "tel:+966598505540";
+}
 }
